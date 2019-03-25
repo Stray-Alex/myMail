daaad9d90f40eabbc8fe33b88f74f8128e22d5f4
Stary-Alex <1005218593@qq.com>
2019-03-25 12:57:13 +0800

    添加 gitignore

--- MainWindow.py
+++ MainWindow.py
@@ -1,3 +1,22 @@
-import  sys
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
-print("我的Email")
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.resize(760, 650)
+        self.setWindowTitle("myMail")
+
+
+
+
+if __name__ == "__main__":
+    import cgitb
+
+    cgitb.enable(format="text")
+    a = QApplication(sys.argv)
+    m = MainWindow()
+    m.show()
+    sys.exit(a.exec_())
