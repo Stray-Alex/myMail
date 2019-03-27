9de009487161d198ffa665b04600239bc5140bf2
Stary-Alex <1005218593@qq.com>
2019-03-27 13:30:46 +0800

    新邮件的三个输入框

--- NewEmail.py
+++ NewEmail.py
@@ -16,14 +16,31 @@ from MenuBar import *
 
 
 class NewEmail(QWidget):
-    def __init__(self,*args,parent=None):
+    def __init__(self, *args, parent=None):
         super().__init__(parent)
         self.setWindowTitle("新邮件")
-        self.resize(760,680)
-        self.move(300,200)
-        self.menu=MenuBar(self)
+        self.resize(760, 700)
+        self.move(300, 50)
+        self.menu = MenuBar(self)
+        self.setFont(QFont("ubuntu", 11))  ##设置字体
 
+        self.name_label = QLabel('收件人:', self)
+        self.name_label.move(20, 50)
+        self.name_input = QLineEdit("", self)
+        self.name_input.move(80, 50)
+        self.name_input.resize(500,25)
 
+        self.subject_label = QLabel('主     题:', self)
+        self.subject_label.move(20, 100)
+        self.subject_input = QLineEdit("", self)
+        self.subject_input.move(80, 100)
+        self.subject_input.resize(500, 25)
+
+        self.passage_label = QLabel("正     文:", self)
+        self.passage_label.move(20, 150)
+        self.passage_input=QTextEdit("",self)
+        self.passage_input.resize(self.width()-120,self.height()-150)
+        self.passage_input.move(80,150)
 
 
 if __name__ == '__main__':
@@ -34,4 +51,3 @@ if __name__ == '__main__':
     m = NewEmail()
     m.show()
     sys.exit(a.exec_())
-
